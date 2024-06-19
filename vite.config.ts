6e79233e9b466f10fc63d482861ac2622565e3b0
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-06-19 18:30:19 +0200

    chore: Update Vite server port to use environment variable

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
+    // port: parseInt((import.meta as any).env.VITE_REACT_PORT),
     port: 8000,
   },
 });
