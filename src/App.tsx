ba4a382bd21837afae181406e153d18459a810da
omrfrkcpr <omerrfarukcapur@gmail.com>
2024-06-19 18:40:45 +0200

    chore: Add axios dependency and implement form submission in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,101 @@
+import { useState } from "react";
+import axios from "axios";
+
+const initialFormState = {
+  title: "",
+  author: "",
+  genre: "",
+  isbn: "",
+  publicationYear: 0,
+  description: "",
+  image: "",
+};
+
 function App() {
-  return <div>Library Management System</div>;
+  const [form, setForm] = useState(initialFormState);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(form);
+    setForm(initialFormState);
+
+    try {
+      await axios.post("/books", form);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      Library Management System`
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            type="text"
+            id="title"
+            name="title"
+            value={form.title}
+            onChange={(e) => setForm({ ...form, title: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="author">Author</label>
+          <input
+            type="text"
+            id="author"
+            name="author"
+            value={form.author}
+            onChange={(e) => setForm({ ...form, author: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="isbn">ISBN</label>
+          <input
+            type="text"
+            id="isbn"
+            name="isbn"
+            value={form.isbn}
+            onChange={(e) => setForm({ ...form, isbn: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="genre">Genre</label>
+          <input
+            type="text"
+            id="genre"
+            name="genre"
+            value={form.genre}
+            onChange={(e) => setForm({ ...form, genre: e.target.value })}
+          />
+        </div>
+        <div>
+          <label htmlFor="publicationYear">Publication Year</label>
+          <input
+            type="number"
+            id="publicationYear"
+            name="publicationYear"
+            value={form.publicationYear}
+            onChange={(e) =>
+              setForm({ ...form, publicationYear: parseInt(e.target.value) })
+            }
+          />
+        </div>
+        <div>
+          <label htmlFor="image">Image</label>
+          <input
+            type="text"
+            id="image"
+            name="image"
+            value={form.image}
+            onChange={(e) => setForm({ ...form, image: e.target.value })}
+          />
+        </div>
+        <button type="submit">Submit Book</button>
+      </form>
+    </div>
+  );
 }
 
 export default App;
